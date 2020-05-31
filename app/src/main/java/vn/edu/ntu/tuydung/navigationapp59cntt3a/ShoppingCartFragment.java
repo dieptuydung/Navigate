be7271a43bb74a4a8f6f0ab070769b1f48a168e9
dieptuydung <63508568+dieptuydung@users.noreply.github.com>
2020-05-31 10:09:26 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/tuydung/navigationapp59cntt3a/ShoppingCartFragment.java
@@ -0,0 +1,27 @@
+package vn.edu.ntu.tuydung.navigationapp59cntt3a;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ShoppingCartFragment extends Fragment {
+
+    public ShoppingCartFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+    }
+}
